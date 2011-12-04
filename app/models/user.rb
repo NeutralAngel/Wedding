999c6f20ea30361f7bc7fb10c00b2feca873e9b8
Nathan Nontell <nathan.nontell@gmail.com>
2011-12-04 18:56:51 -0500

    removed daisy :(

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,5 @@ class User < ActiveRecord::Base
   belongs_to :rsvp
   has_secure_password
   validates_presence_of :password
-  validates :email, :uniqueness => {:message => "login"}
+  validates :email, :uniqueness => {:message => "login"}, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 end
