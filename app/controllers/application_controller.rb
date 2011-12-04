eff885e5b774323642b1603269bb7c0021d14f35
Nathan Nontell <nathan.nontell@gmail.com>
2011-12-03 22:39:20 -0500

    added flower and other shtuff...

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  helper_method :current_user
+  helper_method :current_user, :rsvp_id
 
   private
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
-
+  
+  def rsvp_id
+    user = current_user
+    user.rsvp_id
+  end
 end
