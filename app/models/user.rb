08c32f4da240d036d4820967f257865259c94e24
Nathan Nontell <nathan.nontell@gmail.com>
2011-12-05 18:43:14 -0500

    couple fixes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   belongs_to :rsvp
   has_secure_password
-  validates_presence_of :password
+  validates_presence_of :password, :on => :rsvp#create
   validates :email, :uniqueness => {:message => "login"}, :format => {:with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i}
 end
